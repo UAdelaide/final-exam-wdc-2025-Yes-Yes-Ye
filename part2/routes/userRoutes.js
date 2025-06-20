287d8f1d4b877e472f88e3f6834709819e1566d9
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 15:29:55 +0000

    [automatic commit] 2025-06-20 15:29:55 UTC 49.178.128.60

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -91,7 +91,7 @@ router.get('/dognames', async (req, res) => {
     JOIN Dogs d ON u.user_id = d.owner_id
     WHERE u.user_id = ?
     `, [req.session.user_id]);
-    console.log('Sent the dogs for ',req.session.username, '! The dog(s) are: ', );
+    console.log('Sent the dogs for ',req.session.username, '! The dog(s) are: ', rows[0]);
     res.json(rows);
 });
 
