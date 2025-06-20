8ad5312c0105f65a3e7415323c6faad6cfc40a4a
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 03:21:42 +0000

    [automatic commit] 2025-06-20 03:21:42 UTC 49.178.128.60

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -13,7 +13,7 @@ router.get('/api/dogs', function(req,res,next){
       res.json(rows);
     });
   } catch(error){
-    res.status(404).send('Database not found !');
+    res.status(404).send('Database does not exist or cannot be recognised!');
   }
 });
 
