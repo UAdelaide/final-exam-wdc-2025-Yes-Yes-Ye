cf260d6e3c783296c6c548fad00e56f4beb68108
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 07:38:27 +0000

    [automatic commit] 2025-06-20 07:38:27 UTC 49.178.128.60

--- part2/app.js
+++ part2/app.js
@@ -17,7 +17,7 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 // Add session middleware
 app.use(session({
-    secret: Math.random().toString
+    secret: Math.random().toString(36).substring(2)
 }));
 
 // Export the app instead of listening here
