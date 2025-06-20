25eba2968c5f4a278114264518d25c9d8814655d
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 13:25:29 +0000

    [automatic commit] 2025-06-20 13:25:29 UTC 49.178.128.60

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
+// Created a new route for /api/users/logout 
 router.post('/logout', function(req,res){
   console.log("Logout function activated!");
   req.session.destroy(function(err){
