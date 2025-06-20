c8cd32f7b100c566f677b0baea0fa4691f6d2c71
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 03:42:36 +0000

    [automatic commit] 2025-06-20 03:42:36 UTC 49.178.128.60

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -29,7 +29,7 @@ router.get('/api/walkrequests/open', function(req,res,next){
     db.query(`
       SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
       FROM WalkRequests wr
-      JOIN Users u ON
+      JOIN Dogs d ON 
       `
     );
   } catch(error){}
