3dd572bf517edecd03fe94cc282484727f37265d
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 15:20:14 +0000

    [automatic commit] 2025-06-20 15:20:13 UTC 49.178.128.60

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,7 +88,8 @@ router.get('/dognames', async (req, res) => {
   const [rows] = await db.query(`
     SELECT d.name AS dog_name
     FROM Users u
-    JOIN Dogs d ON ? = 
+    JOIN Dogs d ON u.user_id = d.owner_id
+    
     `
   );
 });
