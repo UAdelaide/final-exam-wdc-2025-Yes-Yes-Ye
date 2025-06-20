7cc6e95453d1fed03bd8de48665757f295e16b90
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 07:37:25 +0000

    [automatic commit] 2025-06-20 07:37:24 UTC ;; communications error to 208.67.222.222#53: timed out 49.178.128.60

--- part2/app.js
+++ part2/app.js
@@ -16,7 +16,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 // Add session middleware
-app.use();
+app.use(session({
+    secret: 
+}));
 
 // Export the app instead of listening here
 module.exports = app;
