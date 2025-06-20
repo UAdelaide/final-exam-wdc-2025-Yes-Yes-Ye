ebdbd84d67f89a49970117878c3921cb9b19aae2
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 04:31:41 +0000

    [automatic commit] 2025-06-20 04:31:41 UTC 49.178.128.60

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -51,7 +51,9 @@ router.get('/api/walkers/summary', function(req,res,next){
       LEFT JOIN WalkRequests wrq ON u.user_id = wrq.walk_id AND wrq.status = 'completed'
       LEFT JOIN WalkRatings wra ON wrq.request_id = wra.request_id
       GROUP BY u.user_id, u.username
-      `
+      `, function(error, rows) {
+        
+      }
     );
   } catch(error){}
 });
