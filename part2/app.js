02ec19562c89f6ff17cfe78ef84c4adb4b7017ea
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 09:01:42 +0000

    [automatic commit] 2025-06-20 09:01:42 UTC 49.178.128.60

--- part2/app.js
+++ part2/app.js
@@ -15,18 +15,6 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-// Add session middleware
-app.use(session({
-    // Basic security measures
-    secret: Math.random().toString(36),
-    resave: false,
-    saveUninitialized: false,
-    // Make it so that the session cookie lasts for an hour
-    cookie: {
-        seure: 'auto',
-        maxAge: 60*60*10000
-    }
-}));
 
 // Export the app instead of listening here
 module.exports = app;
