9b56c9b4bf33e39214d58a9a2dc9c77b05a5219b
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 03:06:26 +0000

    [automatic commit] 2025-06-20 03:06:26 UTC 49.178.128.60

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var messageRouter
+var dogRouter = require('./routes/dogs');
 
 var app = express();
 
@@ -17,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use()
 
 module.exports = app;
