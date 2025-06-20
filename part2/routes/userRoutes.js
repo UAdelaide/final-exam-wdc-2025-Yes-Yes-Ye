6bea3b61559de119959f2e764ca293c0ea9f8e17
Yes Yes ye <a1939771@adelaide.edu.au>
2025-06-20 15:29:14 +0000

    [automatic commit] 2025-06-20 15:29:14 UTC ;; communications error to 208.67.222.222#53: timed out 49.178.128.60

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -91,7 +91,7 @@ router.get('/dognames', async (req, res) => {
     JOIN Dogs d ON u.user_id = d.owner_id
     WHERE u.user_id = ?
     `, [req.session.user_id]);
-    console.log();
+    console.log(req.session.username);
     res.json(rows);
 });
 
